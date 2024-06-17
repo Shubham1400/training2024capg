7cf10cfa22ad029465fdaacc4efb7a4626c352c3
Shubham1400 <73012802+Shubham1400@users.noreply.github.com>
2024-06-17 17:36:51 +0530

    Update a3.c

--- 24may/a3.c
+++ 24may/a3.c
@@ -1,6 +1,21 @@
+/*3. C program to check if a number is an automorphic number (a number whose square ends in the same digits as the 
+number itself).
+Example:
+Consider the number 76.
+1. Calculate the square of the number:
+      76*76=5776
+2. Determine the number of digits in the original number:
+	Number of digits in 76 is 2.
+3. Therefore extract the last 2 digits from the square: (mod of 10 to the power 2)
+       5776 mod 100 = 76
+4. Compare the extracted digits with the original number:
+	Extracted digits: 76
+	Original number: 76
+Since the extracted digits (76) match the original number (76), 76 is an automorphic number.*/
+
 #include <stdio.h>
 #include <math.h>
-
+#include <stdlib.h>
 int main()
 {
     int n;
@@ -19,5 +34,6 @@ int main()
         printf("%d is automorphic",n_copy);
     else 
         printf("%d is not automorphic",n_copy);
-    return 0;
+    
+    return EXIT_SUCCESS;
 }
